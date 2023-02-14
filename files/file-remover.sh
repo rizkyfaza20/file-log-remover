3aab64b75c5e8e3d5aee4c68e83bf4cce45f1e5b
rizkyfaza20 <rizkyfaza61@gmail.com>
2023-02-14 13:05:28 +0700

    added new file : file-remover.sh

---
+++ files/file-remover.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+prev_count=0
+fpath=/var/log/system.log.*.gz
+find $fpath -type f -mtime +7  -exec ls -ltrd {} \; > /tmp/file.out
+find $fpath -type f -mtime +7  -exec rm -rf {} \;
+count=$(cat /tmp/file.out | wc -l)
+if [ "$prev_count" -lt "$count" ] ; then
+MESSAGE="/tmp/file1.out"
+TO="rizki.faza@livecom.com"
+echo "Systems log files are deleted older than 7 days"  >> $MESSAGE
+echo "+--------------------------------------------- +" >> $MESSAGE
+echo "" >> $MESSAGE
+cat /tmp/file.out | awk '{print $6,$7,$9}' >> $MESSAGE
+echo "" >> $MESSAGE
+SUBJECT="WARNING: System log folders are deleted older than 7 days $(date)"
+mail -s "$SUBJECT" "$TO" < $MESSAGE
+rm $MESSAGE /tmp/file.out
+fi
